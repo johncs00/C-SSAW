51174e665cc469c2306b3e03c925b988cb22391e
devrangarajan <rangad@rpi.edu>
2019-11-19 16:49:35 -0500

    Did some twilio stuff, can now recieve messages

---
+++ recv_test.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    from_number = request.values.get('From')
+    print(from_number)
+    body = request.values
+    print(body)
+
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
