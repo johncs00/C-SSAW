202d011ff69088c86425017602bc14f352b8f5fb
Serge P <piskuns16@gmail.com>
2020-04-14 10:51:44 -0700

    basic login stuff

--- Login/loginTest.py
+++ Login/loginTest.py
@@ -1,6 +1,62 @@
 import pymongo
+import hashlib
+import sys
 from pymongo import MongoClient
 
 if __name__ == '__main__':
-	print("Hello world")
+	valid = 0
+	#f = open("temp.txt", "a")
+	client = MongoClient()
+	db = client.test_database
+	users = db.users
+
 	
+	while (valid == 0):
+		d = input("Type L for Login, C for creating account: ")
+		if (d.upper() == "L"):
+			username = input("Username: ")
+			pw = input("Password: ")
+			pw_hash = hashlib.sha256(pw.encode())
+			pw_hash_dig = pw_hash.hexdigest()
+
+			count = db.users.find( { "Name" : username }).count()
+
+			if(count == 0):
+				print("user does not exist")
+				valid = 0
+
+
+
+
+			
+		elif (d.upper() == "C"):
+			#check if username is already taken
+			username = input("Username: ")
+			while (db.users.find( { "Name" : username }).count() != 0):
+				print("Username already taken, try again")
+				username = input("Username: ")
+			#user_hash = hashlib.sha256(username.encode())
+			#hdig = user_hash.hexdigest()
+
+
+			#f.write(hdig)
+			#f.write('\n')
+
+			pw = input("Password: ")
+			pw_hash = hashlib.sha256(pw.encode())
+			pw_hash_dig = pw_hash.hexdigest()
+
+			userAdd = {"Name" : username, "Password" : pw}
+			user_id = users.insert_one(userAdd).inserted_id
+
+			print("Added user " + username + ". Please log in")
+			#f.write(pw_hash.hexdigest())
+			#f.write('\n')
+		elif(d.upper() == "X"):
+			sys.exit()
+		else:
+			print("Invalid selection")
+
+
+	#f.truncate(0)
+	#f.close
